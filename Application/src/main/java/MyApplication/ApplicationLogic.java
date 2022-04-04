7da86a56934dfb20471aa15fad3c1e5b178bef67
Sergei_Bistrov <bistrov-2015@mail.ru>
2022-04-04 17:12:30 +0300

    init

---
+++ Application/src/main/java/MyApplication/ApplicationLogic.java
@@ -0,0 +1,14 @@
+package MyApplication;
+
+public class ApplicationLogic {
+    SimpleConsoleApplication simpleConsoleApplication = new SimpleConsoleApplication();
+        public void runApplication() {
+
+        try {
+            simpleConsoleApplication.runApplication();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
